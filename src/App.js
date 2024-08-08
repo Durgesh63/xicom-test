cf63e7a522b686271f197e44468d137374523c84
durgesh63 <durgeshmaurya0998@gmail.com>
2024-08-09 03:09:39 +0530

    add loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import RegistrationForm from "./RegistrationForm";
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from "./assest/Loader";
+import axios from "axios";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  function apicall() {
+    setLoading(true)
+    axios.get(`${process.env.REACT_APP_BASEURI}/api/v1/is-live`).then(() => {
+      setLoading(false)
+    }).catch((err) => {
+      console.log(err);
+      setLoading(false)
+    })
+  }
+
+  useEffect(() => {
+    apicall()
+  }, [])
+
+
+
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <RegistrationForm />
-    </div>
+    <>
+      {loading && <Loader />}
+
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <RegistrationForm />
+      </div>
+    </>
   );
 }
 
